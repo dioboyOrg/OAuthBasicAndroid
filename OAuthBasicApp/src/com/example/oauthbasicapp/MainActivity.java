a52134084a2d35abba6facfcbc03e663fc7766b1
mirrorball <mirror79@gmail.com>
2014-02-20 16:36:58 +0900

    Google Analytics v3 테스트 코드 추가

--- OAuthBasicApp/src/com/example/oauthbasicapp/MainActivity.java
+++ OAuthBasicApp/src/com/example/oauthbasicapp/MainActivity.java
@@ -17,6 +17,12 @@ import com.example.oauth.OAuthFacebook.OAuthFacebookCallBack;
 import com.example.oauth.OAuthGoogle;
 import com.example.oauth.OAuthGoogle.OAuthGoogleCallBack;
 import com.example.oauth.OAuthUser;
+import com.google.analytics.tracking.android.EasyTracker;
+import com.google.analytics.tracking.android.Fields;
+import com.google.analytics.tracking.android.GAServiceManager;
+import com.google.analytics.tracking.android.GoogleAnalytics;
+import com.google.analytics.tracking.android.MapBuilder;
+import com.google.analytics.tracking.android.Tracker;
 import com.google.common.base.Strings;
 
 public class MainActivity extends Activity {
@@ -31,9 +37,19 @@ public class MainActivity extends Activity {
 	private OAuthGoogle mGoogleLogin = null; 
 	private OAuthFacebook mFacebookLogin = null;
 
+	/** GA ID */
+	private static final String GA_PROPERTY_ID = "UA-48219037-1";
+	private static Tracker mTracker = null;
+	
+	private boolean mUseEasyTracker = true;        // EasyTracker 를 테스트하려면 true 세팅
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+        if (!mUseEasyTracker) {
+            mTracker = GoogleAnalytics.getInstance(this).getTracker(GA_PROPERTY_ID);
+        }
 
 		setContentView(R.layout.activity_main);
 		
@@ -50,6 +66,16 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
                 Log.i("[google]", "google button click");
                 if (mGoogleLogin != null) {
+                    if (mTracker != null) {
+                        mTracker.send(MapBuilder.createEvent("UX", "BtnClick", "GoogleBtn", null)
+                                .build());
+                        GAServiceManager.getInstance().dispatchLocalHits();     // Manual Dispatch for Test (단, 설정하지않으면 default Periodic(30m) Dispatch로 동작함)
+                    } else {
+                        EasyTracker.getInstance(MainActivity.this).send(
+                                MapBuilder.createEvent("Easy_UX", "BtnClick", "GoogleBtn", null)
+                                        .build());
+                        GAServiceManager.getInstance().dispatchLocalHits();     // Manual Dispatch for Test (단, 설정하지않으면 default Periodic(30m) Dispatch로 동작함)
+                    }
                     mGoogleLogin.login();
                 }
             }
@@ -68,7 +94,19 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 Log.i("[facebook]", "facebook button click");
-                mFacebookLogin.login();
+                if (mFacebookLogin != null) {
+                    if (mTracker != null) {
+                        mTracker.send(MapBuilder.createEvent("UX", "BtnClick", "FacebookBtn", null)
+                                .build());
+                        GAServiceManager.getInstance().dispatchLocalHits();     // Manual Dispatch for Test (단, 설정하지않으면 default Periodic(30m) Dispatch로 동작함)
+                    }  else {
+                        EasyTracker.getInstance(MainActivity.this).send(
+                                MapBuilder.createEvent("Easy_UX", "BtnClick", "FacebookBtn", null)
+                                        .build());
+                        GAServiceManager.getInstance().dispatchLocalHits();     // Manual Dispatch for Test (단, 설정하지않으면 default Periodic(30m) Dispatch로 동작함)
+                    }
+                    mFacebookLogin.login();
+                }
             }
 		});
 	}
@@ -80,8 +118,32 @@ public class MainActivity extends Activity {
             mFacebookLogin.onActivityResult(requestCode, resultCode, data);
         }
 	}
+	
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mTracker != null) {
+            mTracker.set(Fields.SCREEN_NAME, "Main Screen");
+            mTracker.send(MapBuilder.createAppView().build());
+        }
+        // [EasyTracker] analytics.xml 에 Screen 이름 설정 추가해서 사용하는 방법 추가되어있음
+        // 아래의 방법은 packagename+activityname 으로 화면이름 생성됨
+        EasyTracker.getInstance(this).activityStart(this);  // Add this method.
+    }
 
-	/**
+     @Override
+    protected void onStop() {
+        super.onStop();
+        if(mTracker != null) {
+            mTracker.set(Fields.SCREEN_NAME, null);
+        }
+        // [EasyTracker] analytics.xml 에 Screen 이름 설정 추가해서 사용하는 방법 추가되어있음
+        // [EasyTracker] xml 에 Screen 이름 설정 추가해서 사용하도록 변경
+        // 아래의 방법은 packagename+activityname 으로 화면이름 생성됨
+        EasyTracker.getInstance(this).activityStop(this);  // Add this method
+    }
+
+    /**
 	 *  app 쉐어드 프레퍼런스에서 값을 읽어옴
 	 * @param context Activity context
 	 * @param key preference key
